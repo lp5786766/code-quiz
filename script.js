90b4c535a249011cdc9ab7f85c38bb856199de3e
Luba Pecheneva <lp5786766@gmail.com>
2020-04-24 14:03:49 -0400

    Started pseudocode, structured html and css files

---
+++ script.js
@@ -0,0 +1,12 @@
+// Coding Quiz intorductory page with "start the quiz button"
+
+//On click question 1 appears with 4 possib;e answers(buttons)
+
+//if you click the right answer you proceed to the next question and the "correct!" message is dispayed
+//if you click on the wrong answer you proceed to the next question and the "wrong!" message is dispayed
+
+//after all the questions your final schore is dispalyed and you are offered to enter your initials and submit.
+
+//then the higschores are dispalyed (1.AB - 22) with two buttons: go back and clear score.
+
+//during the quiz the timer (75s) is ticking and if you answer wrong 10 seconds are taken off the timer. time stops when you finish.
